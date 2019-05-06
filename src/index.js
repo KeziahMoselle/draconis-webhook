b410f27ae57baea742b1158d7b36514a474b6ae0
KeziahMoselle <keziah.moselle@gmail.com>
2019-05-06 17:39:34 +0200

    feat: switch to webhook instead of discord bot

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 require('dotenv').config()
-import Discord from 'discord.js'
 import getGuildEvents from './libs/getGuildEvents'
 import generateEmbed from './libs/generateEmbed'
 
-const client = new Discord.Client()
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`)
-})
-
-client.on('message', async (message) => {
-  if (message.content === '!events') {
-    const events = await getGuildEvents()
-    events.forEach(event => {
-      const embed = generateEmbed(event.title, event.date, process.env.GUILD_THUMBNAIL_URL)
-      message.channel.send(`Nouvel événement pour <${process.env.GUILD_NAME}>`, { embed })
-    })
-  }
-})
-
-client.login(process.env.DISCORD_CLIENT_ID)
+// Crawl for new events here
+// Send a POST request to the WebHook URL if a new event appear
--- src/libs/generateEmbed.js
+++ src/libs/generateEmbed.js
@@ -1,18 +1,25 @@
-function generateEmbed (title, date, img) {
- return {
-    "color": 16736617,
-    "thumbnail": {
-      "url": img
-    },
-    "fields": [
+require('dotenv').config()
+
+function generateEmbed (title, date, img = process.env.GUILD_THUMBNAIL_URL) {
+  return {
+    "content": `Nouvel événement pour <${process.env.GUILD_NAME}>`,
+    "embeds": [
       {
-        "name": "Event",
-        "value": title
-      },
-      {
-        "name": "Date",
-        "value": date
-      }
+        "color": 16736617,
+        "thumbnail": {
+          "url": img
+        },
+        "fields": [
+          {
+            "name": "Event",
+            "value": title
+          },
+          {
+            "name": "Date",
+            "value": date
+          }
+        ]
+      }	
     ]
   }
 }
