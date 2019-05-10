3c40da90c52c51501269fe51ebfdc8fc352806c9
KeziahMoselle <keziahmoselle@protonmail.com>
2019-05-10 18:20:03 +0200

    feat: change color and icon based on type (#2)
    close #2

--- src/libs/generateEmbed.mjs
+++ src/libs/generateEmbed.mjs
@@ -2,11 +2,24 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 function generateEmbed (title, date, img, thumbnail = process.env.GUILD_THUMBNAIL_URL) {
+  let color = 16777215
+  // Raid + Dungeon
+  if (img.includes('/wow/assets/lfg/')) {
+    color = 9442302
+    img = 'https://wow.zamimg.com/images/wow/icons/large/inv_enchant_voidsphere.jpg'
+  }
+  // Other
+  if (img.includes('/wow/assets/calendar/ui-calendar-event-other.png')) color = 16042892
+  // PvP
+  if (img.includes('/wow/assets/calendar/ui-calendar-event-pvp01.png')) color = 13893636
+  // Meeting
+  if (img.includes('/wow/assets/calendar/calendar_meetingstart.png')) color = 4886754
+
   return {
     "content": `Nouvel événement pour <${process.env.GUILD_NAME}>`,
     "embeds": [
       {
-        "color": 16736617,
+        "color": color,
         "thumbnail": {
           "url": img
         },
