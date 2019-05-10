74323f2e075197a492bc8bbc3128f195acbf12f0
KeziahMoselle <keziahmoselle@protonmail.com>
2019-05-10 15:15:27 +0200

    feat: detect more guild event types (#1)
    close #1

--- src/libs/getGuildEvents.mjs
+++ src/libs/getGuildEvents.mjs
@@ -1,15 +1,16 @@
 import getEvents from './getEvents.mjs'
 
-
 async function getGuildEvents () {
   let guildEvents = []
   const events = await getEvents()
 
   events.forEach(event => {
-    /* 
-     * /wow/assets/lfg/ is only used on custom events
-     */
-    if (event.img.includes('/wow/assets/lfg/')) {
+    if (
+      event.img.includes('/wow/assets/lfg/') ||
+      event.img.includes('/wow/assets/calendar/ui-calendar-event-other.png') ||
+      event.img.includes('/wow/assets/calendar/ui-calendar-event-pvp01.png') ||
+      event.img.includes('/wow/assets/calendar/calendar_meetingstart.png')
+    ) {
       guildEvents.push(event)
     }
   })
